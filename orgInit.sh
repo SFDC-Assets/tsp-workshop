900877e90070b1e351da00488456682f05fc0245
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:02:46 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,4 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -w60 -p admin -e transaction.workshop
-sfdx shane tsp username update
 sf project deploy start
 sf org assign permset -n TransactionSecurity
 sf demoutil user password set -p salesforce1 -g User -l User
